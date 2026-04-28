37adf0f4c0cafb219cd49fabfc7083dc6673ea1f
nipunikarunanayake <nipunikarunanayake2@gmail.com>
2026-04-28 08:29:29 +0530

    fixed error

--- server.js
+++ server.js
@@ -24,3 +24,7 @@ mongoose.connect(process.env.MONGO_URI)
   .catch(err => {
     console.log("Database connection error:", err);
   });
+
+  app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
